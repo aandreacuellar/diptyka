75d76fee6fcbe0ace872f8344c69b84eb6310271
aandreacuellar <paula.cuellar@usantotomas.edu.co>
2025-05-29 15:34:13 -0500

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -12,4 +12,39 @@ cerrarmenu.addEventListener("click", () => {
 	navmenu.classList.remove("visible");
 })
 
-// BUSCADOR
+
+// TRANSICIÓN
+
+//gsap.registerPlugin(ScrollTrigger);
+//
+//let panels = gsap.utils.toArray(".panel");
+//let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
+//
+//panels.forEach((panel, i) => {
+//  ScrollTrigger.create({
+//    trigger: panel,
+//    start: () => panel.offsetHeight < window.innerHeight ? "top top" : "bottom bottom", // if it's shorter than the viewport, we prefer to pin it at the top
+//    pin: true, 
+//    pinSpacing: false 
+//  });
+//});
+//
+//ScrollTrigger.create({
+//  snap: {
+//    snapTo: (progress, self) => {
+//      let panelStarts = tops.map(st => st.start),
+//          snapScroll = gsap.utils.snap(panelStarts, self.scroll());
+//    },
+//    duration: 0.5
+//  }
+//})
+//ScrollTrigger.create({
+//  snap: {
+//    snapTo: (progress, self) => {
+//      let panelStarts = tops.map(st => st.start),
+//          snapScroll = gsap.utils.snap(panelStarts, self.scroll());
+//      return gsap.utils.normalize(0, ScrollTrigger.maxScroll(window), snapScroll);
+//    },
+//    duration: 0.5
+//  }
+//});
