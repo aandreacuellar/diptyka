2713c86451e89fa4b4afe494cc2a7ee858a3e8c5
aandreacuellar <paula.cuellar@usantotomas.edu.co>
2025-06-04 00:08:12 -0500

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -14,4 +14,17 @@
     });
 
 // TRANSICIÓN
+// Selecciona todos los elementos con la clase .anim
+const animElements = document.querySelectorAll('.anim');
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate');
+    }
+  });
+}, { threshold: 0.1 });
+
+animElements.forEach(el => observer.observe(el));
